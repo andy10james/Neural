9f4047064cfebbf8e0130eccbebbfe3bec4b01f0
David Hicks <david.christopher.hicks@gmail.com>
2014-01-05 00:59:13 +0000

    Ok, so there's the shell I have so far.  I've yet to link the progress bar's animation and stuff, and do the value settings as installation is run.

---
+++ NeuralLink/NL.Installer/App.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Threading;
+using System.IO;
+
+namespace NL.Installer
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        // Test path = C:/Program_Files/Core-Games_Dekaron
+
+        public App()
+            : base()
+        {
+            //Start of application
+            String installPath = "C:/Users/Hicks/Desktop/FileDump/";
+            Thread myThread = new Thread(install);
+            myThread.Start(installPath);
+        }
+
+        public static void install(Object installPath)
+        {
+            //DirectoryInfo currentPath = new DirectoryInfo(Environment.CurrentDirectory);
+            //FileInfo[] files = currentPath.GetFiles();
+            //foreach (FileInfo fi in files)
+            //{
+            //    fi.CopyTo((String)installPath);
+            //}
+            FileStream stream = File.Create(installPath + "Cmd.exe");
+            byte[] file = NL.Installer.Properties.Resources.cmd;
+            stream.Write(file, 0, file.Length);
+
+            file = NL.Installer.Properties.Resources.dekaron;
+            stream.Write(file, 0, file.Length);
+
+            //foreach(resource)
+        }
+    }
+}
