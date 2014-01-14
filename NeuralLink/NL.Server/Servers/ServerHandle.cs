b39f70fc1963dceaf0d99d4f76655a803024be94
Andrew James <me@andyjames.it>
2014-01-14 23:41:50 +0000

    NL-1 CLient connection handling is now done in a new class; ServerHandle. Class has an onDeath event.

--- NeuralLink/NL.Server/Servers/ServerHandle.cs
+++ NeuralLink/NL.Server/Servers/ServerHandle.cs
@@ -1,14 +1,91 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Text;
 using System.Net.Sockets;
 using System.Net;
+using NL.Server.Configuration;
 using System.Threading;
+using NL.Server.View;
+using NL.Server.Controllers;
+using NL.Common;
 
 namespace NL.Server.Servers {
     internal class ServerHandle {
-        public Thread Thread;
-        public TcpClient Client;
+
+        public delegate void OnDeathEvent(ServerHandle handle);
+        public event OnDeathEvent OnDeath;
+        public IPEndPoint EndPoint;
+
+        private readonly IRemoteController _controller;
+        private readonly TcpClient _client;
+        private Boolean ended = false;
+
+        
+
+
+        public ServerHandle(IRemoteController controller, TcpClient client) {
+            this._controller = controller;
+            this._client = client;
+            this.EndPoint = client.Client.RemoteEndPoint as IPEndPoint;
+        }
+
+        public void Start() {
+            Thread handler = new Thread(Handle);
+            handler.Name = Thread.CurrentThread.Name + "." + (_client.Client.RemoteEndPoint as IPEndPoint);
+            handler.Start();
+        }
+
+        private void Handle() {
+
+            if (EndPoint == null) {
+                _client.Close();
+                return;
+            }
+
+            if (ServerConfiguration.BeepOnConnection) {
+                Console.Beep(1000, 80);
+                Console.Beep(1500, 80);
+                Console.Beep(2000, 200);
+            }
+
+            IPAddress ipaddress = EndPoint.Address;
+            NetworkStream clientStream = _client.GetStream();
+
+            NLConsole.WriteLine(String.Format(_controller.ClientConnectedMessage, ipaddress), _controller.ClientConnectedColor);
+
+            int bytesRead = 0;
+            Byte[] messageBytes = new Byte[64];
+            Boolean complete = false;
+
+            while (!ended) {
+                try { bytesRead = clientStream.Read(messageBytes, 0, messageBytes.Length); }
+                catch { NLConsole.WriteLine(String.Format(_controller.ClientTerminatedMessage, ipaddress), _controller.ClientTerminatedColor); break; }
+                if (bytesRead == 0) { complete = true; break; }
+                String message = Encoding.ASCII.GetString(messageBytes, 0, bytesRead);
+                CommandPattern command = CommandPattern.Create(message);
+                _controller.InvokeAction(command, _client);
+            }
+
+            _client.Close();
+
+            if (complete) {
+                NLConsole.WriteLine(String.Format(_controller.ClientDisconnectedMessage, ipaddress), _controller.ClientDisconnectedColor);
+            }
+
+            if (ServerConfiguration.BeepOnDisconnection) {
+                Console.Beep(2000, 80);
+                Console.Beep(1500, 80);
+                Console.Beep(1000, 200);
+            }
+
+
+
+        }
+
+        public void Die() {
+            ended = true;
+            this._client.Close();
+        }
+
     }
 }
--- NeuralLink/NL.Server/Servers/ServerManager.cs
+++ NeuralLink/NL.Server/Servers/ServerManager.cs
@@ -45,7 +45,7 @@ namespace NL.Server.Servers {
         public void Disconnect() {
             if (IsAlive) {
                 foreach (ServerHandle handle in _handles) {
-                    handle.Client.Close();
+                    handle.Die();
                 }
                 _listener.Stop();
             }
@@ -53,90 +53,33 @@ namespace NL.Server.Servers {
             NLConsole.WriteLine(message, ConsoleColor.Cyan);
         }
 
+
+        public Int32 DisconnectIP(IPAddress ipaddress) {
+            Int32 disconnected = 0;
+            foreach (ServerHandle handle in _handles.Where(h => h.EndPoint.Address.Equals(ipaddress))) {
+                handle.Die();
+                disconnected++;
+            }
+            return disconnected;
+        }
+        
         private void Listen() {
             _listener = new TcpListener(IPAddress.Any, _port);
             _listener.Start();
             while (true) {
                 TcpClient client;
-                try { client = _listener.AcceptTcpClient(); }
-                catch (Exception e) {
+                try { client = _listener.AcceptTcpClient(); } catch (Exception e) {
                     if (!(e is SocketException) || (e as SocketException).ErrorCode != 10004) {
                         String message = String.Format(Strings.ServerAbnormallyTerminated, _port, e.Message + "(" + e.Source + ")");
                         NLConsole.WriteLine(message, ConsoleColor.Red);
                     }
                     break;
                 }
-                Thread handler = new Thread(HandleClient);
-                handler.Name = Thread.CurrentThread.Name + "." + (client.Client.RemoteEndPoint as IPEndPoint);
-                handler.Start(client);
+                ServerHandle handle = new ServerHandle(_server, client);
+                handle.OnDeath += (ServerHandle e) => this._handles.Remove(e);
+                handle.Start();
             }
         }
-
-        public void HandleClient(Object clientObject) {
-
-            TcpClient client = (TcpClient)clientObject;
-            IPEndPoint endPoint = client.Client.RemoteEndPoint as IPEndPoint;
-
-            if (endPoint == null) {
-                client.Close();
-                return;
-            }
-
-            ServerHandle handle = new ServerHandle {
-                Thread = Thread.CurrentThread,
-                Client = client
-            };
-            _handles.Add(handle);
-
-            if (ServerConfiguration.BeepOnConnection) {
-                Console.Beep(1000, 80);
-                Console.Beep(1500, 80);
-                Console.Beep(2000, 200);
-            }
-           
-            IPAddress ipaddress = endPoint.Address;
-            NetworkStream clientStream = client.GetStream();
-
-            NLConsole.WriteLine(String.Format(_server.ClientConnectedMessage, ipaddress), _server.ClientConnectedColor);
-
-            int bytesRead = 0;
-            Byte[] messageBytes = new Byte[64];
-            Boolean complete = false;
-
-            while (true) {
-                try { bytesRead = clientStream.Read(messageBytes, 0, messageBytes.Length); } 
-                catch { NLConsole.WriteLine(String.Format(_server.ClientTerminatedMessage, ipaddress), _server.ClientTerminatedColor); break; }
-                if (bytesRead == 0) { complete = true; break; }
-                String message = Encoding.ASCII.GetString(messageBytes, 0, bytesRead);
-                CommandPattern command = CommandPattern.Create(message);
-                _server.InvokeAction(command, client);
-            }
-
-            if (complete) {
-                NLConsole.WriteLine(String.Format(_server.ClientDisconnectedMessage, ipaddress), _server.ClientDisconnectedColor);
-            }
-
-            client.Close();
-
-            _handles.Remove(handle);
-
-            if (ServerConfiguration.BeepOnDisconnection) {
-                Console.Beep(2000, 80);
-                Console.Beep(1500, 80);
-                Console.Beep(1000, 200);
-            }
-
-        }
-
-        public Int32 DisconnectIP(IPAddress ipaddress) {
-            Int32 disconnected = 0;
-            foreach (ServerHandle handle in _handles.Where(h => (h.Client.Client.RemoteEndPoint as IPEndPoint).Address.Equals(ipaddress))) {
-                handle.Client.Close();
-                disconnected++;
-            }
-            return disconnected;
-        }
-
 
     }
 }
